b83d74945c5762b5965d9eb00e2f67cb01cf8ab1
Harun Eren Özkaya <harunerenozkaya@gmail.com>
2023-11-06 22:38:48 +0300

    fix pointer remains bug

--- src/main/java/com/harun/liveSlide/components/pdfViewer/PDFPageContainer.java
+++ src/main/java/com/harun/liveSlide/components/pdfViewer/PDFPageContainer.java
@@ -14,6 +14,7 @@ public class PDFPageContainer extends Group {
         super(pdfPage);
         this.pdfPage = pdfPage;
         this.getChildren().add(pointerShape);
+        this.pointerShape.setVisible(false);
     }
 
     public PDFPage getPDFPage(){
