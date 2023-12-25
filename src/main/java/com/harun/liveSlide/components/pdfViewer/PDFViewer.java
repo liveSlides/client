2cde512496d9d9788da2629a3e99d0f476405c24
Harun Eren Özkaya <harunerenozkaya@gmail.com>
2023-12-25 20:30:31 +0300

    fix2

--- src/main/java/com/harun/liveSlide/components/pdfViewer/PDFViewer.java
+++ src/main/java/com/harun/liveSlide/components/pdfViewer/PDFViewer.java
@@ -209,7 +209,7 @@ public class PDFViewer extends BorderPane {
 
     public void loadPDF(String path) {
         pdfViewerFileController.loadPDF(path , 1 , true);
-        //pdfViewerObserver.loadedPDF(path);
+        pdfViewerObserver.loadedPDF(path);
     }
 
     public void loadPDF(String path , int index) throws IOException {
